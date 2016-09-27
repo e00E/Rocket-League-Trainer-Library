f2a9ad6ca7cec19df6ed6bcd3b561504ac891c99
e00E <vakevk+git@gmail.com>
2016-09-27 19:04:01 +0200

    File output, variable tick rate

--- Examples/Speed monitor/Source.cpp
+++ Examples/Speed monitor/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include "Rocket League Trainer Library.h"
 
 #include <thread>
@@ -15,17 +16,41 @@ int main()
 		signature_scan(car, ball);
 		//std::cout << "ball pos " << std::hex << reinterpret_cast<size_t>(ball + signature_position_offset) << "\n";
 		//std::cout << "car pos " << std::hex << reinterpret_cast<size_t>(car + signature_position_offset) << "\n";
-		unsigned int sleep_milliseconds(50);
-		std::cout << "Reading speed every " << sleep_milliseconds << "ms" << "\n";
+
+		int64_t nanosleeptime(0);
+		std::cout << "Enter tick length in nano seconds: ";
+		std::cin >> nanosleeptime;
+		std::chrono::nanoseconds sleep_time(nanosleeptime); //128 tickrate would be 7812500 nano seconds.
+		double sleep_seconds = nanosleeptime / 1000000000.0;
+		std::cout << "Reading speed every " << sleep_seconds << "s" << "\n";
+
+		std::ofstream output("speed log.txt", std::ios_base::trunc);
+		output << "Read interval: " << sleep_seconds << "s" << "\n\n";
+
+		//double last_speed1(0);
+		//double last_speed2(0);
+
+		std::chrono::time_point<std::chrono::high_resolution_clock> now(std::chrono::high_resolution_clock::now());
 
 		while (true) {
 			entity e(get_entity_from_signature(car));
-			float speed1 = std::sqrt(std::pow(e.velocity.x, 2) + std::pow(e.velocity.y, 2) + std::pow(e.velocity.z, 2));
-			float speed2 = std::sqrt(std::pow(e.velocity.x, 2) + std::pow(e.velocity.y, 2));
-			std::cout << "speed on x y z axis: " << std::fixed << std::setprecision(2) << e.velocity.x << " " << e.velocity.y << " " << e.velocity.z << "\n"
-				<< "= total speed: " << speed1 << "\n"
-				<< "without z: " << speed2 << "\n" << std::endl;
-			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_milliseconds));
+			//std::cout << std::unitbuf;
+			double speed1 = std::sqrt(std::pow(e.velocity.x, 2) + std::pow(e.velocity.y, 2) + std::pow(e.velocity.z, 2));
+			double speed2 = std::sqrt(std::pow(e.velocity.x, 2) + std::pow(e.velocity.y, 2));
+
+			//double acceleration1((speed1 - last_speed1) / sleep_seconds);
+			//double acceleration2((speed2 - last_speed2) / sleep_seconds);
+
+			std::cout << std::fixed << std::setprecision(std::numeric_limits<long double>::digits10 + 1) << "Speed:\n" << "x y z: " << e.velocity.x << " " << e.velocity.y << " " << e.velocity.z << "\n"
+				<< "Total with z: " << speed1 << "\nWithout z   : " << speed2 << "\n"
+				/*<< "Acceleration to last measure:\n" << "With z : " << acceleration1 << "\nWithout: " << acceleration2 << "\n" */<< std::endl;
+			output << std::fixed << std::setprecision(8) << "Speed:\n" << "x y z: " << e.velocity.x << " " << e.velocity.y << " " << e.velocity.z << "\n"
+				<< "Total with z: " << speed1 << "\nWithout z   : " << speed2 << "\n"
+				/*<< "Acceleration to last measure:\n" << "With z : " << acceleration1 << "\nWithout: " << acceleration2 << "\n" */<< std::endl;
+			//last_speed1 = speed1;
+			//last_speed2 = speed2;
+			now += sleep_time;
+			std::this_thread::sleep_until(now);
 		}
 
 	}
